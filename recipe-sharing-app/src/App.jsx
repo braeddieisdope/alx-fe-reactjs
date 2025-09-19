8628d7e0c253b5d004f7b3b37feecc39dce9c9ad
Edmund Tekpenor <slicegamen@gmail.com>
2025-09-19 16:36:04 +0000

    search added

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,12 +3,12 @@ import { Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar'; // Import the new component
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* [Router] */}
       <nav>
         <Link to="/">Home</Link>
       </nav>
@@ -16,6 +16,7 @@ function App() {
         <Route path="/" element={
           <>
             <h1>Recipe Sharing App</h1>
+            <SearchBar /> {/* Add SearchBar component here */}
             <AddRecipeForm />
             <h2>My Recipes</h2>
             <RecipeList />
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,15 +1,45 @@
+
 import create from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (recipeId) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== recipeId) })),
+  searchTerm: '',
+  filteredRecipes: [],
+
+  setSearchTerm: (term) => set(state => {
+    // Update searchTerm and immediately re-filter recipes
+    const newFilteredRecipes = state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(term.toLowerCase())
+    );
+    return {
+      searchTerm: term,
+      filteredRecipes: newFilteredRecipes
+    };
+  }),
+
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe],
+    filteredRecipes: [...state.recipes, newRecipe] // Add new recipe to filtered list too
+  })),
+
+  deleteRecipe: (recipeId) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== recipeId),
+    filteredRecipes: state.filteredRecipes.filter(recipe => recipe.id !== recipeId)
+  })),
+
   updateRecipe: (updatedRecipe) => set(state => ({
     recipes: state.recipes.map(recipe =>
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+    filteredRecipes: state.filteredRecipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     )
   })),
-  setRecipes: (recipes) => set({ recipes })
+  
+  setRecipes: (recipes) => set(state => ({ 
+    recipes: recipes,
+    filteredRecipes: recipes // Initialize filtered list with all recipes
+  })),
 }));
 
 export { useRecipeStore };
