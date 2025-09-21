3aa962ee33540aa97d56acb91de79d479bc025c4
Edmund Tekpenor <slicegamen@gmail.com>
2025-09-21 08:57:11 +0000

    modified

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,7 @@
 
 import axios from 'axios';
 
-const GITHUB_SEARCH_API_URL = 'https://api.github.com/search/users';
+const GITHUB_SEARCH_API_URL = 'https://api.github.com/search/users?q';
 const GITHUB_USER_API_URL = 'https://api.github.com/users/';
 
 export const fetchUserData = async (username) => {
